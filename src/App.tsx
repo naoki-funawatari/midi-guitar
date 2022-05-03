b3443653e21e9c52442073c6a9d9c921fa4b32f3
naoki-funawatari <g011c1180@gmail.com>
2022-05-03 12:12:13 +0900

    feat: コンポーネント分割、音程を修正。

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import fingerboard from "@/assets/fingerboard";
+import Fingerboard from "@/features/Fingerboard";
 import "@/assets/style.scss";
 
-// http://var.blog.jp/archives/84027358.html
-// http://curtaincall.weblike.jp/portfolio-web-sounder/webaudioapi-basic/oscillator
-const sound = (frequency: number) => () => {
-  const audioContext = new AudioContext();
-  const gainNode = audioContext.createGain();
-  gainNode.gain.value = 0.1;
-  gainNode.connect(audioContext.destination);
-  const oscillator = audioContext.createOscillator();
-  oscillator.type = "sine";
-  oscillator.frequency.value = frequency;
-  oscillator.detune.value = 300;
-  oscillator.connect(gainNode);
-  // https://www.g200kg.com/jp/docs/webaudio/oscillator.html
-  oscillator.start();
-  oscillator.stop(0.3);
-};
-
-const useGuitar = () => {
-  const strings = Object.entries(fingerboard);
-  strings.sort((a, b) => b[0].localeCompare(a[0]));
-  return { strings };
-};
-
 export default function App() {
-  const { strings } = useGuitar();
-  return (
-    <div className="fingerboard">
-      <div className="string">
-        <div>&nbsp;</div>
-        {[...Array(23).keys()].map(o => (
-          <div key={o} className="fret fret-no">
-            {o}
-          </div>
-        ))}
-      </div>
-      {strings.map(string => {
-        const [stringNo, frets] = string;
-        const key = `strings-${stringNo}`;
-        return (
-          <div key={key} className="string">
-            <div>{stringNo}弦</div>
-            {frets.map(fret => {
-              const key = `strings-${stringNo}-flet-${fret.id}`;
-              return (
-                <div key={key} className="fret">
-                  <button onClick={sound(fret.frequency)}>{fret.ja}</button>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <Fingerboard />;
 }
