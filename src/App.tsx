d745c2614f4983b50bec136462fb8f8ff62aa99c
naoki-funawatari <g011c1180@gmail.com>
2022-05-03 10:56:47 +0900

    feat: とりあえず音が出るようになった。

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
+import scale from "@/assets/scale";
 import "@/assets/style.scss";
 
+// http://var.blog.jp/archives/84027358.html
+// http://curtaincall.weblike.jp/portfolio-web-sounder/webaudioapi-basic/oscillator
+const sound = (frequency: number) => {
+  const audioContext = new AudioContext();
+  const gainNode = audioContext.createGain();
+  gainNode.gain.value = 0.1;
+  gainNode.connect(audioContext.destination);
+  const oscillator = audioContext.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = frequency;
+  oscillator.detune.value = 300;
+  oscillator.connect(gainNode);
+  // https://www.g200kg.com/jp/docs/webaudio/oscillator.html
+  oscillator.start();
+  oscillator.stop(0.3);
+};
+
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [volume, setVolume] = useState(0.1);
+
   return (
     <div>
       <div className="red">hello world!</div>
-      <div>{count}</div>
+      <div>{volume}</div>
       <div>
-        <button onClick={() => setCount(state => state + 1)}>カウント</button>
+        <button onClick={() => setVolume(state => state + 0.1)}>
+          ボリューム＋
+        </button>
+        <button onClick={() => setVolume(state => state - 0.1)}>
+          ボリューム－
+        </button>
+      </div>
+      <div>
+        {scale.map(s => (
+          <button key={s.id} onClick={() => sound(s.frequency)}>
+            {s.name}
+          </button>
+        ))}
       </div>
     </div>
   );
