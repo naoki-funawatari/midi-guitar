974467699c30cebf483634687b29194d77ad9722
naoki-funawatari <g011c1180@gmail.com>
2022-05-17 18:13:22 +0900

    feat: モードスケールのデータを追加。

--- src/features/scales/hooks.ts
+++ src/features/scales/hooks.ts
@@ -11,67 +11,51 @@ export const useScale = () => {
     [root]
   );
   const intervals = {
-    major: [0, 2, 2, 1, 2, 2, 2, 1],
-    minor: [0, 2, 1, 2, 2, 1, 2, 2],
+    ionian: [0, 2, 2, 1, 2, 2, 2, 1],
+    dorian: [0, 2, 1, 2, 2, 2, 1, 2],
+    phrygian: [0, 1, 2, 2, 2, 1, 2, 2],
+    lydian: [0, 2, 2, 2, 1, 2, 2, 1],
+    mixolydian: [0, 2, 2, 1, 2, 2, 1, 2],
+    aeolian: [0, 2, 1, 2, 2, 1, 2, 2],
+    locrian: [0, 1, 2, 2, 1, 2, 2, 2],
     majorPentatonic: [0, 2, 2, 3, 2, 3],
     minorPentatonic: [0, 3, 2, 2, 3, 2],
   };
-  const majorScale = useMemo(() => {
+
+  const createScale = (interval: number[]) => {
     const scale: { tone: ITone; position: number }[] = [];
-    intervals.major.reduce((prev, curr) => {
+
+    interval.reduce((prev, curr) => {
       const index = prev + curr;
       scale.push({
         tone: temp[index],
         position: root.current - root.default + index + 1,
       });
+
       return prev + curr;
     }, 0);
 
     return scale;
-  }, [intervals.major, temp, root]);
-  const minorScale = useMemo(() => {
-    const scale: { tone: ITone; position: number }[] = [];
-    intervals.minor.reduce((prev, curr) => {
-      const index = prev + curr;
-      scale.push({
-        tone: temp[index],
-        position: root.current - root.default + index + 1,
-      });
-      return prev + curr;
-    }, 0);
+  };
 
-    return scale;
-  }, [intervals.minor, temp, root]);
-  const majorPentatonicScale = useMemo(() => {
-    const scale: { tone: ITone; position: number }[] = [];
-    intervals.majorPentatonic.reduce((prev, curr) => {
-      const index = prev + curr;
-      scale.push({
-        tone: temp[index],
-        position: root.current - root.default + index + 1,
-      });
-      return prev + curr;
-    }, 0);
-
-    return scale;
-  }, [intervals.majorPentatonic, temp, root]);
-  const minorPentatonicScale = useMemo(() => {
-    const scale: { tone: ITone; position: number }[] = [];
-    intervals.minorPentatonic.reduce((prev, curr) => {
-      const index = prev + curr;
-      scale.push({
-        tone: temp[index],
-        position: root.current - root.default + index + 1,
-      });
-      return prev + curr;
-    }, 0);
-
-    return scale;
-  }, [intervals.minorPentatonic, temp, root]);
+  const majorScale = createScale(intervals.ionian);
+  const dorianScale = createScale(intervals.dorian);
+  const phrygianScale = createScale(intervals.phrygian);
+  const lydianScale = createScale(intervals.lydian);
+  const mixolydianScale = createScale(intervals.mixolydian);
+  const minorScale = createScale(intervals.aeolian);
+  const locrianScale = createScale(intervals.locrian);
+  const majorPentatonicScale = createScale(intervals.majorPentatonic);
+  const minorPentatonicScale = createScale(intervals.minorPentatonic);
 
   return {
     majorScale,
+    dorianScale,
+    phrygianScale,
+    lydianScale,
+    mixolydianScale,
     minorScale,
+    locrianScale,
     majorPentatonicScale,
     minorPentatonicScale,
   };
